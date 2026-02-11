6174728fe3f01b084833121c8c025ab31941cbef
k1468sy-design <k1468sy@gmail.com>
2026-02-11 23:41:18 +0900

    Update Index.js

--- Index.js
+++ Index.js
@@ -46,4 +46,8 @@ app.post("/", (req, res) => {
   });
 });
 
-app.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
+
